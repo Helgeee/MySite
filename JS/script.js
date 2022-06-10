0a5fd379ddb51df08380d5e7540e2f4566610fbd
Helgeee <olegvotoz@gmail.com>
2022-06-10 23:10:27 +0300

    Add Foto.jpg

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,2 @@
-window.onload = function () {
-    document.getElementById("menu").onclick = function(){
-    var x = document.getElementById('myTopnav');
-    if (x.className === "topnav") {
-    x.className += " responsive";
-    } else {
-    x.className = "topnav";
-    }
-    }
-};
+
 
